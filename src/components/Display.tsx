ac10e6d55d318e7927b440dd228472d339f2f1d0
bob13am <bob.ibravic@gmail.com>
2025-07-30 08:52:13 -0400

    ..

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -7,7 +7,7 @@ interface DisplayProps {
 function Display(displayProps: DisplayProps) {
    return (
      <>
-        <div style={{ marginTop: '60px', marginLeft: '120px',padding: '10px',border: '2px solid #333',borderRadius: '10px',background: '#000000ff',fontWeight: 'bold',fontSize: '2rem',textAlign: 'center',width: '80px',
+        <div style={{ marginTop: '60px', marginLeft: '10px',padding: '10px',border: '2px solid #333',borderRadius: '10px',background: '#000000ff',fontWeight: 'bold',fontSize: '2rem',textAlign: 'center',width: '80px',
     boxShadow: '0 2px 8px rgba(255, 255, 255, 1)'}}>{displayProps.total}</div>
      </>
    );
